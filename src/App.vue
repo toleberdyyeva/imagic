0ce35759706493b3bf7f529a7533f7b6aa700ba0
Alisher <fredcolin079@gmail.com>
2018-07-02 23:25:15 +0600

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div style="width:200px;">
+      <imagic/>
+    </div>
+  </div>
+</template>
+<script>
+import Imagic from './components/Imagic'
+
+export default {
+  name: 'App',
+  components: {
+    Imagic
+  }
+}
+</script>
+
+<style>
+  *{
+    font-family: Arial, Helvetica, sans-serif;
+  }
+</style>
---
+++ src/components/Imagic.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="imagic">  
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'imagic',
+  data () {
+    return {
+    
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.imagic{
+  background-color: #e5e5e5;
+  width: 100%;
+  padding-top: 100%;
+}
+</style>
